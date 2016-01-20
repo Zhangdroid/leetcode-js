5674e96bd1adbcedc801b67808b64dc72408b664
张卓 <contrary.zhuo@gmail.com>
2016-01-20 18:27:39 +0800

    add "two sum"🐰

---
+++ two-sum.js
@@ -0,0 +1,24 @@
+// Given an array of integers, find two numbers such that they add up to a specific target number.
+
+// The function twoSum should return indices of the two numbers such that they add up to the target, where index1 must be less than index2. Please note that your returned answers (both index1 and index2) are not zero-based.
+
+// You may assume that each input would have exactly one solution.
+
+// Input: numbers={2, 7, 11, 15}, target=9
+// Output: index1=1, index2=2
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+	for (var i = 0; i < nums.length; i++) {
+		var tempNum = target - nums[i];
+		for (var j = i + 1; j < nums.length; j++) {
+			if (nums[j] === tempNum) {
+				return [i + 1, j + 1];
+			}
+		}
+	}
+};
